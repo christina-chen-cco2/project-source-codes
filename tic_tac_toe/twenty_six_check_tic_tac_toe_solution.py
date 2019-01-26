2088c5351e356009e58f0c29a7aa6c34465daac6
Christina Chen <46726195+christina-chen-cco2@users.noreply.github.com>
2019-01-25 14:45:18 -1000

    Update twenty_six_check_tic_tac_toe_solution.py

--- tic_tac_toe/twenty_six_check_tic_tac_toe_solution.py
+++ tic_tac_toe/twenty_six_check_tic_tac_toe_solution.py
@@ -2,14 +2,12 @@ def checker(board_list, move_squares_list, board_size):
 
     rows = int(board_size[0])
     columns = int(board_size[3])
-
-    j = 0
     one_score = 0
     two_score = 0
 
     # check by rows
-    for i in range(len(move_squares_list)):
-        for j in range(columns - 1):
+    for i in range(0, len(move_squares_list), columns):
+        for j in range(columns):
             if board_list[move_squares_list[j + i]] == 'X':
                 one_score += 1
                 if one_score == columns:
@@ -21,19 +19,14 @@ def checker(board_list, move_squares_list, board_size):
         one_score = 0
         two_score = 0
 
-
-    j = 0
-    one_score = 0
-    two_score = 0
-
     # check by columns
-    for i in range(columns):
-        for j in range(i, rows, rows):
-            if board_list[move_squares_list[j]] == 'X':
+    for i in range(0, columns):
+        for j in range(0, len(move_squares_list), columns):
+            if board_list[move_squares_list[j + i]] == 'X':
                 one_score += 1
                 if one_score == columns:
                     return 1
-            elif board_list[move_squares_list[j]] == 'O':
+            elif board_list[move_squares_list[j + i]] == 'O':
                 two_score += 1
                 if two_score == columns:
                     return 2
@@ -45,6 +38,20 @@ def checker(board_list, move_squares_list, board_size):
 
     # check by top-left to bottom-right diagonal
     for i in range(0, len(move_squares_list), (1 + columns)):
+        if board_list[move_squares_list[i]] == 'X':
+            one_score += 1
+            if one_score == columns:
+                return 1
+        elif board_list[move_squares_list[i]] == 'O':
+            two_score += 1
+            if two_score == columns:
+                return 2
+
+    one_score = 0
+    two_score = 0
+
+    # check by top-right to bottom-left diagonal
+    for i in range((columns - 1), len(move_squares_list), (columns - 1)):
         if board_list[move_squares_list[i]] == 'X':
             one_score += 1
             if one_score == columns:
